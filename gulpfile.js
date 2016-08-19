1ab45de4ad466f8ebc7e7919daa9e9338e66f4e4
Yeepin Yheng <yeepin@gmail.com>
2016-08-18 17:29:15 -0700

    minify everything

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var minifyCss = require('gulp-minify-css')
+var sourceMaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+var imageMin = require('gulp-imagemin');
+
+gulp.task('default', function(){
+  browserSync.init({
+    server: './'
+  });
+  gulp.watch('src/styles/**/*.css', ['styles']);
+  gulp.watch('src/scripts/**/*.js', ['scripts']);
+  gulp.watch('src/img/**/*', ['images']);
+  gulp.watch('*.html', browserSync.reload);
+});
+
+gulp.task('images', function(){
+    gulp.src(['src/img/**/*'])
+      .pipe(imageMin())
+      .pipe(gulp.dest('dist/img'))
+      .pipe(browserSync.stream());
+});
+
+gulp.task('scripts', function(){
+  gulp.src(['src/scripts/main.js'])
+    .pipe(sourceMaps.init())
+    .pipe(uglify())
+    .pipe(sourceMaps.write())
+    .pipe(gulp.dest('dist/scripts'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('styles', function() {
+  gulp.src(['src/styles/**/*.css'])
+    .pipe(sourceMaps.init())
+    .pipe(minifyCss())
+    .pipe(sourceMaps.write())
+    .pipe(gulp.dest('dist/styles'))
+    .pipe(browserSync.stream());
+});
